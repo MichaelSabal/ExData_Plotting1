20743c03f453d940bd10d9d6951853e0086b528d
MichaelSabal <mike@msabal.net>
2016-07-20 20:07:37 -0400

    This crazy American didn't realize the dates were in European format.

--- plot1.R
+++ plot1.R
@@ -1,7 +1,7 @@
 plot1 <- function() {
 	pc <- read.table("household_power_consumption.txt",sep=";",
 		colClasses=c("character","character",rep("numeric",7)),header=TRUE,na.strings="?")
-	pc <- pc[pc$Date=="2/1/2007" | pc$Date=="2/2/2007",]
+	pc <- pc[pc$Date=="1/2/2007" | pc$Date=="2/2/2007",]
 	png("plot1.png",width=480,height=480)
 	hist(pc$Global_active_power,breaks = 18,col="orangered3",
 		 xlab="Global Active Power (kilowatts)",main="Global Active Power")
--- plot2.R
+++ plot2.R
@@ -1,9 +1,9 @@
 plot2 <- function() {
 	pc <- read.table("household_power_consumption.txt",sep=";",
 		colClasses=c("character","character",rep("numeric",7)),header=TRUE,na.strings="?")
-	pc <- pc[pc$Date=="2/1/2007" | pc$Date=="2/2/2007",]
+	pc <- pc[pc$Date=="1/2/2007" | pc$Date=="2/2/2007",]
 	png("plot2.png",width=480,height=480)
-	plot(strptime(paste(pc[,1],pc[,2]),format="%m/%d/%Y %H:%M:%S"),
+	plot(strptime(paste(pc[,1],pc[,2]),format="%d/%m/%Y %H:%M:%S"),
 		 pc$Global_active_power,type="l",ylab="Global Active Power (kilowatts)",xlab="")
 	dev.off()
 }
